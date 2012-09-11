b6e69f40e5d5eb077371df9f3f977cf4a5836f43
Josh Cartmell <joshcartme@gmail.com>
2012-09-11 13:24:18 -0700

    Made models optional baesd upon settings.

--- mezztend/content/models.py
+++ mezztend/content/models.py
@@ -1,50 +1,54 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from mezzanine.conf import settings
 from mezzanine.core.fields import FileField
 from mezzanine.core.models import RichText, Slugged
 from mezzanine.pages.models import Page
 
 from mezztend.core.models import TwoColumnRichText
 
-class ContentBlock(RichText, Slugged):
-    """
-    Provides a block of rich text content which can be placed in a
-    template using the associated render_content_block tag
-    """
-    description = models.CharField(max_length=100)
-    image = FileField(upload_to="contentblocks", format="Image",
-                      max_length=255, null=True, blank=True,
-                      help_text="Optional, may be used in certain "
-                                "circumstances and not others")
+if "contentblock" in settings.MEZZTEND_CONTENT_MODELS:
+    class ContentBlock(RichText, Slugged):
+        """
+        Provides a block of rich text content which can be placed in a
+        template using the associated render_content_block tag
+        """
+        description = models.CharField(max_length=100)
+        image = FileField(upload_to="contentblocks", format="Image",
+                          max_length=255, null=True, blank=True,
+                          help_text="Optional, may be used in certain "
+                                    "circumstances and not others")
+        
+        class Meta:
+            db_table = "mezztend_content_contentblock"
+        
+        def __unicode__(self):
+            return self.title
+
+if "menuitem" in settings.MEZZTEND_CONTENT_MODELS:
+    class MenuItem(Page):
+        """
+        A general content type for creating menu items which have no content
+        associated with them.
+        """
     
-    class Meta:
-        db_table = "mezztend_content_contentblock"
+        class Meta:
+            verbose_name = _("Menu item")
+            verbose_name_plural = _("Menu items")
     
-    def __unicode__(self):
-        return self.title
+        def get_absolute_url(self):
+            return '#'
 
-class MenuItem(Page):
-    """
-    A general content type for creating menu items which have no content
-    associated with them.
-    """
-
-    class Meta:
-        verbose_name = _("Menu item")
-        verbose_name_plural = _("Menu items")
-
-    def get_absolute_url(self):
-        return '#'
-
-class TwoColumnRichTextPage(Page, TwoColumnRichText):
-    """
-    Implements a page with two Rich Text content fields
-    """
-    
-    class Meta:
-        db_table = "mezztend_content_twocolumnrichtextpage"
-        verbose_name = _("Two column rich text page")
-        verbose_name_plural = _("Two column rich text pages")
+if "twocolumnrichtextpage" in settings.MEZZTEND_CONTENT_MODELS:
+    class TwoColumnRichTextPage(Page, TwoColumnRichText):
+        """
+        Implements a page with two Rich Text content fields
+        """
+        
+        class Meta:
+            db_table = "mezztend_content_twocolumnrichtextpage"
+            verbose_name = _("Two column rich text page")
+            verbose_name_plural = _("Two column rich text pages")
     
     
