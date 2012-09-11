82c7ecc4f55e3564372f046e4ea28bbd115a35a0
Josh Cartmell <joshcartme@gmail.com>
2012-09-11 13:39:13 -0700

    Further improved dynamic admin handling.

--- mezztend/content/admin.py
+++ mezztend/content/admin.py
@@ -6,32 +6,28 @@ from mezzanine.pages.admin import PageAdmin, page_fieldsets
 
 from models import MODELS
 
-if "contentblock" in MODELS:
+if not MODELS or "contentblock" in MODELS:
     from models import ContentBlock
-if "menuitem" in MODELS:
-    from models import MenuItem
-if "twocolumnrichtextpage" in MODELS:
-    from models import TwoColumnRichTextPage
-
-class ContentBlockAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'slug')
-    fields = ('title', 'description', 'content', 'image', 'slug')
-    readonly_fields = ('slug',)
-
-
-# Drop the meta data fields, and move slug towards the stop.
-menu_item_fieldsets = deepcopy(page_fieldsets[:1])
-menu_item_fieldsets[0][1]["fields"] = menu_item_fieldsets[0][1]["fields"][:-1]
-menu_item_fieldsets[0][1]["fields"].insert(1, "slug")
-
-
-class MenuItemAdmin(PageAdmin):
-
-    fieldsets = menu_item_fieldsets
-
-if "contentblock" in MODELS:
+    
+    class ContentBlockAdmin(admin.ModelAdmin):
+        list_display = ('title', 'description', 'slug')
+        fields = ('title', 'description', 'content', 'image', 'slug')
+        readonly_fields = ('slug',)
     admin.site.register(ContentBlock, ContentBlockAdmin)
-if "menuitem" in MODELS:
+    
+if not MODELS or "menuitem" in MODELS:
+    from models import MenuItem
+    
+    # Drop the meta data fields, and move slug towards the stop.
+    menu_item_fieldsets = deepcopy(page_fieldsets[:1])
+    menu_item_fieldsets[0][1]["fields"] = menu_item_fieldsets[0][1]["fields"][:-1]
+    menu_item_fieldsets[0][1]["fields"].insert(1, "slug")
+    
+    class MenuItemAdmin(PageAdmin):
+        fieldsets = menu_item_fieldsets
     admin.site.register(MenuItem, MenuItemAdmin)
-if "twocolumnrichtextpage" in MODELS:
+
+if not MODELS or "twocolumnrichtextpage" in MODELS:
+    from models import TwoColumnRichTextPage
+    
     admin.site.register(TwoColumnRichTextPage, PageAdmin)
