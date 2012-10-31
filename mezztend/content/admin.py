98f466679fc3339ede94f41076fcb3efcb027007
Josh Cartmell <joshcartme@gmail.com>
2012-10-31 10:43:50 -0700

    Removed reliance on setting to determine if an app should be installed as it may have conflicted with South.  Undesired apps should be unregistered with the admin.  Created migrations.

--- mezztend/content/admin.py
+++ mezztend/content/admin.py
@@ -4,30 +4,25 @@ from django.contrib import admin
 
 from mezzanine.pages.admin import PageAdmin, page_fieldsets
 
-from models import MODELS
+from models import ContentBlock, MenuItem, TwoColumnRichTextPage
 
-if not MODELS or "contentblock" in MODELS:
-    from models import ContentBlock
-    
-    class ContentBlockAdmin(admin.ModelAdmin):
-        list_display = ('title', 'description', 'slug')
-        fields = ('title', 'description', 'content', 'image', 'href', 'slug')
-        readonly_fields = ('slug',)
-    admin.site.register(ContentBlock, ContentBlockAdmin)
-    
-if not MODELS or "menuitem" in MODELS:
-    from models import MenuItem
-    
-    # Drop the meta data fields, and move slug towards the top.
-    menu_item_fieldsets = deepcopy(page_fieldsets[:1])
-    menu_item_fieldsets[0][1]["fields"] = menu_item_fieldsets[0][1]["fields"][:-1]
-    menu_item_fieldsets[0][1]["fields"].insert(1, "href")
-    
-    class MenuItemAdmin(PageAdmin):
-        fieldsets = menu_item_fieldsets
-    admin.site.register(MenuItem, MenuItemAdmin)
+class ContentBlockAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'slug')
+    fields = ('title', 'description', 'content', 'image', 'href', 'slug')
+    readonly_fields = ('slug',)
+admin.site.register(ContentBlock, ContentBlockAdmin)
 
-if not MODELS or "twocolumnrichtextpage" in MODELS:
-    from models import TwoColumnRichTextPage
-    
-    admin.site.register(TwoColumnRichTextPage, PageAdmin)
+
+from models import MenuItem
+
+# Drop the meta data fields, and move slug towards the top.
+menu_item_fieldsets = deepcopy(page_fieldsets[:1])
+menu_item_fieldsets[0][1]["fields"] = menu_item_fieldsets[0][1]["fields"][:-1]
+menu_item_fieldsets[0][1]["fields"].insert(1, "href")
+
+class MenuItemAdmin(PageAdmin):
+    fieldsets = menu_item_fieldsets
+admin.site.register(MenuItem, MenuItemAdmin)
+
+
+admin.site.register(TwoColumnRichTextPage, PageAdmin)
--- mezztend/content/models.py
+++ mezztend/content/models.py
@@ -8,60 +8,54 @@ from mezzanine.pages.models import Page
 
 from mezztend.core.models import TwoColumnRichText
 
-try:
-    MODELS = settings.MEZZTEND_CONTENT_MODELS
-except AttributeError:
-    MODELS = None
-
-if not MODELS or "contentblock" in MODELS:
-    class ContentBlock(RichText, Slugged):
-        """
-        Provides a block of rich text content which can be placed in a
-        template using the associated render_content_block tag
-        """
-        description = models.CharField(max_length=100)
-        image = FileField(upload_to="contentblocks", format="Image",
-                          max_length=255, null=True, blank=True,
-                          help_text="Optional, may be used in certain "
-                                    "circumstances and not others")
-        href = models.CharField(max_length=500, blank=True,
-            help_text="An optional link for the image or otherwise.")
-        
-        class Meta:
-            db_table = "mezztend_content_contentblock"
-        
-        def __unicode__(self):
-            return self.title
-
-if not MODELS or "menuitem" in MODELS:
-    class MenuItem(Page):
-        """
-        A general content type for creating menu items which have no content
-        associated with them.
-        """
-        href = models.CharField(max_length=500, blank=True,
-            help_text="If blank this will simply sit in the menus, otherwise it will "
-                      "link to whatever you specify here.")
-        
-        class Meta:
-            db_table = "mezztend_content_menuitem"
-            verbose_name = _("Menu item")
-            verbose_name_plural = _("Menu items")
+class ContentBlock(RichText, Slugged):
+    """
+    Provides a block of rich text content which can be placed in a
+    template using the associated render_content_block tag
+    """
+    description = models.CharField(max_length=100)
+    image = FileField(upload_to="contentblocks", format="Image",
+                      max_length=255, null=True, blank=True,
+                      help_text="Optional, may be used in certain "
+                                "circumstances and not others")
+    href = models.CharField(max_length=500, blank=True,
+        help_text="An optional link for the image or otherwise.")
     
-        def get_absolute_url(self):
-            if self.href:
-                return self.href
-            return '#'
+    class Meta:
+        db_table = "mezztend_content_contentblock"
+    
+    def __unicode__(self):
+        return self.title
 
-if not MODELS or "twocolumnrichtextpage" in MODELS:
-    class TwoColumnRichTextPage(Page, TwoColumnRichText):
-        """
-        Implements a page with two Rich Text content fields
-        """
-        
-        class Meta:
-            db_table = "mezztend_content_twocolumnrichtextpage"
-            verbose_name = _("Two column rich text page")
-            verbose_name_plural = _("Two column rich text pages")
+
+class MenuItem(Page):
+    """
+    A general content type for creating menu items which have no content
+    associated with them.
+    """
+    href = models.CharField(max_length=500, blank=True,
+        help_text="If blank this will simply sit in the menus, otherwise it will "
+                  "link to whatever you specify here.")
+    
+    class Meta:
+        db_table = "mezztend_content_menuitem"
+        verbose_name = _("Menu item")
+        verbose_name_plural = _("Menu items")
+
+    def get_absolute_url(self):
+        if self.href:
+            return self.href
+        return '#'
+
+
+class TwoColumnRichTextPage(Page, TwoColumnRichText):
+    """
+    Implements a page with two Rich Text content fields
+    """
+    
+    class Meta:
+        db_table = "mezztend_content_twocolumnrichtextpage"
+        verbose_name = _("Two column rich text page")
+        verbose_name_plural = _("Two column rich text pages")
     
     
