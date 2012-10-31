4724bdcd66b21bda1cd210058674ed066ea14eff
Josh Cartmell <joshcartme@gmail.com>
2012-10-31 11:15:51 -0700

    Created custom page get_absolute_url to properly create the urls for menu items.

--- mezztend/content/models.py
+++ mezztend/content/models.py
@@ -1,4 +1,7 @@
+from copy import deepcopy
+
 from django.db import models
+from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 
 from mezzanine.conf import settings
@@ -47,6 +50,15 @@ class MenuItem(Page):
             return self.href
         return '#'
 
+original_get_absolute_url = deepcopy(Page.get_absolute_url) 
+def menu_item_get_absolute_url(self):
+    """
+    Returns a menu's href if this is a menu, otherwise, returns as normal
+    """
+    if self.content_model == "menuitem":
+        return self.get_content_model().get_absolute_url()
+    return original_get_absolute_url(self)
+Page.get_absolute_url = menu_item_get_absolute_url
 
 class TwoColumnRichTextPage(Page, TwoColumnRichText):
     """
