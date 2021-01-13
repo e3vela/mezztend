62d865016393ccb013019b298bf958ab5e83121e
Eduardo Vela <eduardo3vela@gmail.com>
2021-01-13 15:35:37 +0100

    Mezzanine 4 update - Make app compatible with Django 1.8

--- mezztend/content/migrations/0001_initial.py
+++ mezztend/content/migrations/0001_initial.py
@@ -1,108 +1,61 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import mezzanine.core.fields
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'ContentBlock'
-        db.create_table('mezztend_content_contentblock', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('site', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['sites.Site'])),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=500)),
-            ('slug', self.gf('django.db.models.fields.CharField')(max_length=2000, null=True, blank=True)),
-            ('content', self.gf('mezzanine.core.fields.RichTextField')()),
-            ('description', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('image', self.gf('mezzanine.core.fields.FileField')(max_length=255, null=True, blank=True)),
-            ('href', self.gf('django.db.models.fields.CharField')(max_length=500, blank=True)),
-        ))
-        db.send_create_signal('content', ['ContentBlock'])
+    dependencies = [
+        ('pages', '0005_add_sites_field'),
+        ('sites', '0001_initial'),
+    ]
 
-        # Adding model 'MenuItem'
-        db.create_table('mezztend_content_menuitem', (
-            ('page_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['pages.Page'], unique=True, primary_key=True)),
-            ('href', self.gf('django.db.models.fields.CharField')(max_length=500, blank=True)),
-        ))
-        db.send_create_signal('content', ['MenuItem'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'ContentBlock'
-        db.delete_table('mezztend_content_contentblock')
-
-        # Deleting model 'MenuItem'
-        db.delete_table('mezztend_content_menuitem')
-
-
-    models = {
-        'content.contentblock': {
-            'Meta': {'object_name': 'ContentBlock', 'db_table': "'mezztend_content_contentblock'"},
-            'content': ('mezzanine.core.fields.RichTextField', [], {}),
-            'description': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'href': ('django.db.models.fields.CharField', [], {'max_length': '500', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'image': ('mezzanine.core.fields.FileField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['sites.Site']"}),
-            'slug': ('django.db.models.fields.CharField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '500'})
-        },
-        'content.menuitem': {
-            'Meta': {'ordering': "('_order',)", 'object_name': 'MenuItem', 'db_table': "'mezztend_content_menuitem'", '_ormbases': ['pages.Page']},
-            'href': ('django.db.models.fields.CharField', [], {'max_length': '500', 'blank': 'True'}),
-            'page_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['pages.Page']", 'unique': 'True', 'primary_key': 'True'})
-        },
-        'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        'generic.assignedkeyword': {
-            'Meta': {'ordering': "('_order',)", 'object_name': 'AssignedKeyword'},
-            '_order': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'keyword': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'assignments'", 'to': "orm['generic.Keyword']"}),
-            'object_pk': ('django.db.models.fields.IntegerField', [], {})
-        },
-        'generic.keyword': {
-            'Meta': {'object_name': 'Keyword'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['sites.Site']"}),
-            'slug': ('django.db.models.fields.CharField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '500'})
-        },
-        'pages.page': {
-            'Meta': {'ordering': "('titles',)", 'object_name': 'Page'},
-            '_meta_title': ('django.db.models.fields.CharField', [], {'max_length': '500', 'null': 'True', 'blank': 'True'}),
-            '_order': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
-            'content_model': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True'}),
-            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'expiry_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
-            'gen_description': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'in_menus': ('mezzanine.pages.fields.MenusField', [], {'default': '[1, 2, 3]', 'max_length': '100', 'null': 'True', 'blank': 'True'}),
-            'keywords_string': ('django.db.models.fields.CharField', [], {'max_length': '500', 'blank': 'True'}),
-            'login_required': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': "orm['pages.Page']"}),
-            'publish_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
-            'short_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
-            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['sites.Site']"}),
-            'slug': ('django.db.models.fields.CharField', [], {'max_length': '2000', 'null': 'True', 'blank': 'True'}),
-            'status': ('django.db.models.fields.IntegerField', [], {'default': '2'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
-            'titles': ('django.db.models.fields.CharField', [], {'max_length': '1000', 'null': 'True'})
-        },
-        'sites.site': {
-            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
-            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        }
-    }
-
-    complete_apps = ['content']
+    operations = [
+        migrations.CreateModel(
+            name='ContentBlock',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=500, verbose_name='Title')),
+                ('slug', models.CharField(help_text='Leave blank to have the URL auto-generated from the title.', max_length=2000, verbose_name='URL', blank=True)),
+                ('content', mezzanine.core.fields.RichTextField(verbose_name='Content')),
+                ('description', models.CharField(max_length=100)),
+                ('image', mezzanine.core.fields.FileField(help_text=b'Optional, may be used in certain circumstances and not others', max_length=255, null=True, blank=True)),
+                ('href', models.CharField(help_text=b'An optional link for the image or otherwise.', max_length=500, blank=True)),
+                ('sites', models.ManyToManyField(to='sites.Site')),
+            ],
+            options={
+                'db_table': 'mezztend_content_contentblock',
+            },
+        ),
+        migrations.CreateModel(
+            name='MenuItem',
+            fields=[
+                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='pages.Page')),
+                ('href', models.CharField(help_text=b'If blank this will simply sit in the menus, otherwise it will link to whatever you specify here.', max_length=500, blank=True)),
+            ],
+            options={
+                'ordering': ('_order',),
+                'db_table': 'mezztend_content_menuitem',
+                'verbose_name': 'Menu item',
+                'verbose_name_plural': 'Menu items',
+            },
+            bases=('pages.page',),
+        ),
+        migrations.CreateModel(
+            name='TwoColumnRichTextPage',
+            fields=[
+                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='pages.Page')),
+                ('left_column', mezzanine.core.fields.RichTextField(verbose_name='Left column')),
+                ('right_column', mezzanine.core.fields.RichTextField(verbose_name='Right column')),
+            ],
+            options={
+                'ordering': ('_order',),
+                'db_table': 'mezztend_content_twocolumnrichtextpage',
+                'verbose_name': 'Two column rich text page',
+                'verbose_name_plural': 'Two column rich text pages',
+            },
+            bases=('pages.page', models.Model),
+        ),
+    ]
